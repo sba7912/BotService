0f4c088d5e1492526b7d27b5a77da288c890d83b
sba7912 <33753143+sba7912@users.noreply.github.com>
2017-11-17 21:59:38 +0900

    2017/11/17 22:00 1st

---
+++ miyadai_1fhAZkHSZUK/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=miyadai-Web-Deploy.pubxml -p:Password=DGnx4ddf7Cp4RwssiaGFv4bpHvyMgCrRb4m8sRSqJwowWzNekz4Wp7vxqX95
+
