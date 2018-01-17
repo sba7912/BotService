9fa2e4fb80a0bbd72c39cf1a43b585ae8f5ee4f5
sba7912 <33753143+sba7912@users.noreply.github.com>
2018-01-18 00:45:22 +0900

    2018/1/18 0:45

--- miyadai_1fhAZkHSZUK/Dialogs/BasicLuisDialog.cs
+++ miyadai_1fhAZkHSZUK/Dialogs/BasicLuisDialog.cs
@@ -204,7 +204,7 @@ namespace Microsoft.Bot.Sample.LuisBot
         }
 
       
-
+        
 
         // Go to https://luis.ai and create a new intent, then train/publish your luis app.
         // Finally replace "MyIntent" with the name of your newly created intent in the following handler
