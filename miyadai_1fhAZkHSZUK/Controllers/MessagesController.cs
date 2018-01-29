bd2728f97c72fd1545b768f860248e1436338c37
sba7912 <33753143+sba7912@users.noreply.github.com>
2018-01-30 02:01:02 +0900

    2018/1/30 2:00

--- miyadai_1fhAZkHSZUK/Controllers/MessagesController.cs
+++ miyadai_1fhAZkHSZUK/Controllers/MessagesController.cs
@@ -359,7 +359,7 @@ namespace Microsoft.Bot.Sample.LuisBot
                                 if (activity.Text == item.v)
                                 {
                                     //replyToConversation = menuFunc2(activity, getMenu3List(Menu1Select, item.i));
-                                    //await LUIS(activity);
+                                   await LUIS(activity);
                                     //メニュー階層2で何番を選んだか保存
                                     userData.SetProperty<int>("Menu2Select", item.i);
 
